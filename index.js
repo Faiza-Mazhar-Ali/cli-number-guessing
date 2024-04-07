1aa6824a0c4980a05eaa1f43ccde73adc09ad33d
Faiza-Mazhar-Ali <rizwanali2k18114@gmainl.com>
2024-04-07 06:25:30 +0500

    Create a number guessing game

---
+++ index.js
@@ -0,0 +1,19 @@
+import inquirer from "inquirer";
+// 1) Computer will generate a random number -
+// 2) User input for number-guessing game -
+// 3) Compair the user input with computer generated number and show the result -
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please inter any number from 1 to 6!",
+    },
+]);
+//if else condition
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("Congractulations! You have guessed the right number.");
+}
+else {
+    console.log("Sorry! You have guessed the wrong number.");
+}
